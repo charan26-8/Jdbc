ec02987663679e1ea212242e03456c5d9c72d9d8
Malli Charan <113853116+charan26-8@users.noreply.github.com>
2024-04-15 14:38:12 +0530

    Update DBConnection.java

--- JDBC/src/DBConnection.java
+++ JDBC/src/DBConnection.java
@@ -1,15 +1,15 @@
-import java.sql.*;
+import java.sql.*;//step-1:import the packages 
 public class DBConnection {
 	public static void main(String[] args) throws Exception {
-	Class.forName("com.mysql.jdbc.Driver");  
+	Class.forName("com.mysql.jdbc.Driver");  //step-2:load the jar file and register : This method is used to dynamically load the driver class.
 	Connection con=DriverManager.getConnection(  
-	"jdbc:mysql://localhost:3308/classicmodels","root","root");  
-	//here sonoo is database name, root is username and password  
-	Statement stmt=con.createStatement();  
-	ResultSet rs=stmt.executeQuery("select * from employees") ; 
+	"jdbc:mysql://localhost:3308/classicmodels","root","root"); step-3://The getConnection() method of DriverManager class is used to establish connection with the database. 
+	//here classicmodels is database name, root is username and password  
+	Statement stmt=con.createStatement();//Step-4:The object of statement is responsible to execute queries with the database.  
+	ResultSet rs=stmt.executeQuery("select * from employees") ; //step-5:This method returns the object of ResultSet that can be used to get all the records of a table.
 	while(rs.next())  
 	System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
-	con.close();  
+	con.close();  //step-6:The close() method of Connection interface is used to close the connection.
 	
 
 }
